6e1910de9fbdd34ac6e56e2a6d4e5f832927be08
pawel <pawel@4nol74.cloud>
2023-07-09 20:10:07 +0200

    Refactoring to mariadb

--- tut09.py
+++ tut09.py
@@ -13,8 +13,6 @@ DB_STRING = "my.db"
 
 secrets=dotenv_values(".env")
 
-print(secrets)
-
 try:
     mariadbconn = mariadb.connect(
         user=secrets["user"],
@@ -29,62 +27,73 @@ except mariadb.Error as e:
 
 mariadbcur = mariadbconn.cursor()
 
-mariadbcur.execute("CREATE TABLE `python_test`.`user_string` (`session_id` INT NOT NULL , `value` INT NOT NULL ) ENGINE = InnoDB;")
+try:
+    test = mariadbcur.execute("SELECT session_id FROM user_string;")
+except:
+    print("Tabela user_string nie istnieje")
+    print("Tworzę")
+    mariadbcur.execute("CREATE TABLE `python_test`.`user_string` (`session_id` TEXT NOT NULL , `value` TEXT NOT NULL ) ENGINE = InnoDB;")
+
+# mariadbcur.execute("INSERT INTO user_string (session_id, value) VALUES (?, ?)", ("Maria", "db"))
+# mariadbconn.commit()
 
 class StringGenerator(object):
     @cherrypy.expose
     def index(self):
         return open('index.html')
 
-
 @cherrypy.expose
 class StringGeneratorWebService(object):
 
     @cherrypy.tools.accept(media='text/plain')
     def GET(self):
-        with sqlite3.connect(DB_STRING) as c:
+        with mariadbconn.cursor() as c:
             cherrypy.session['ts'] = time.time()
             r = c.execute("SELECT value FROM user_string WHERE session_id=?",
                           [cherrypy.session.id])
-            return r.fetchone()
+        mariadbconn.commit()
+        return r.fetchone()
 
     def POST(self, length=8):
         some_string = ''.join(random.sample(string.hexdigits, int(length)))
-        with sqlite3.connect(DB_STRING) as c:
+        with mariadbconn.cursor() as c:
             cherrypy.session['ts'] = time.time()
-            c.execute("INSERT INTO user_string VALUES (?, ?)",
+            c.execute("INSERT INTO user_string (session_id, value) VALUES (?, ?)",
                       [cherrypy.session.id, some_string])
+        mariadbconn.commit()
         return some_string
 
     def PUT(self, another_string):
-        with sqlite3.connect(DB_STRING) as c:
+        with mariadbconn.cursor() as c:
             cherrypy.session['ts'] = time.time()
             c.execute("UPDATE user_string SET value=? WHERE session_id=?",
                       [another_string, cherrypy.session.id])
+        mariadbconn.commit()
 
     def DELETE(self):
         cherrypy.session.pop('ts', None)
-        with sqlite3.connect(DB_STRING) as c:
+        with mariadbconn.cursor() as c:
             c.execute("DELETE FROM user_string WHERE session_id=?",
                       [cherrypy.session.id])
+        mariadbconn.commit()
 
 
-def setup_database():
-    """
-    Create the `user_string` table in the database
-    on server startup
-    """
-    with sqlite3.connect(DB_STRING) as con:
-        con.execute("CREATE TABLE user_string (session_id, value)")
+# def setup_database():
+#     """
+#     Create the `user_string` table in the database
+#     on server startup
+#     """
+#     with sqlite3.connect(DB_STRING) as con:
+#         con.execute("CREATE TABLE user_string (session_id, value)")
 
 
-def cleanup_database():
-    """
-    Destroy the `user_string` table from the database
-    on server shutdown.
-    """
-    with sqlite3.connect(DB_STRING) as con:
-        con.execute("DROP TABLE user_string")
+# def cleanup_database():
+#     """
+#     Destroy the `user_string` table from the database
+#     on server shutdown.
+#     """
+#     with sqlite3.connect(DB_STRING) as con:
+#         con.execute("DROP TABLE user_string")
 
 
 if __name__ == '__main__':
@@ -104,8 +113,8 @@ if __name__ == '__main__':
         }
     }
 
-    cherrypy.engine.subscribe('start', setup_database)
-    cherrypy.engine.subscribe('stop', cleanup_database)
+    # cherrypy.engine.subscribe('start', setup_database)
+    # cherrypy.engine.subscribe('stop', cleanup_database)
 
     webapp = StringGenerator()
     webapp.generator = StringGeneratorWebService()
